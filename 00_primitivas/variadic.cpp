05be9ea78bd573801458347cde97141b0003753c
cristian100110 <cristiandavid100110@gmail.com>
2017-11-14 11:18:05 +0100

    numero

--- 00_primitivas/variadic.cpp
+++ 00_primitivas/variadic.cpp
@@ -1,28 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ROJO "\x1B[31m"
-#define NORM "\x1B[39m"
-
 int main(){
 
-    int entero = 34;
-    float realillo = 35.7; // Ha caido en desgracia.
-    double real = 35.7;
-    unsigned char byte = 97;
-    long int entero_largo = 93;
-    unsigned positivo = 5;
+int op1 = 7;
+int op2;
 
-printf("%i  \t(%lu bytes)\n", entero, sizeof(int));
-printf("%li  \t(%lu bytes)\n", entero_largo, sizeof(int));
-printf("%u  \t(%lu bytes)\n", positivo, sizeof(int));
+printf("Operando 2: ");
+scanf(" %i", &op2);
 
-printf("%i \t(%lu bytes)\n", byte, sizeof(char));
-
-printf("%f  \t(%lu bytes)\n", realillo, sizeof(float));
-printf("%8.2lf \t(%lu bytes)\n", real, sizeof(double));        // long float
-printf(ROJO "12\n" NORM);
-
+printf("%i + %i = %i\n", op1, op2, op1 + op2 );
 
 return EXIT_SUCCESS;
 
