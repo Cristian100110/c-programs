3990fc49b6677bea56a105770dd489426a0158eb
root <cristian100110>
2018-01-17 20:33:18 +0100

    Prueba

--- clase/asteriscos.cpp
+++ clase/asteriscos.cpp
@@ -1,21 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 5
+#define N 7
 int main(){
-    for (int f=0; f<7; f++){
+
+ /* 
+    for (int f=0; f<7; f++){ fila
+            printf("***");
+        printf("\n");
+    
+    for (int c=0; c<7; c++) columna
             printf("*");
         printf("\n");
-    }
-    for (int f=0; f<N; f++){
-            printf("       *");
-        printf("\n");
-    }
-    for (int f=0; f<7; f++){
-            printf(" *")
-    }
+    } 
+  */
+
+printf(" Ajedrez\n");
+
+for(int f=0; f<N; f++){
+   for (int c=0; c<N; c++)
+        if (f==0, f==N-1, c==0,c==N-1) 
+          printf(" ");
+        else 
+          printf("*");
+        printf(" *");
+	printf("\n");
+
+/*for (int f=0; f>3; f++)
+   for (int c=0; c=N; c++)
+        printf(" ");
+        printf("\n");*/
+}
 
 
+    printf("\n");
 
     return EXIT_SUCCESS;
 
