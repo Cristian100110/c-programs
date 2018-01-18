8218920078a8c245119439f59c2d1b9142319416
cristian100110 <cristiandavid100110@gmail.com>
2018-01-18 13:27:02 +0100

    18ENE

--- clase/asteriscos.cpp
+++ clase/asteriscos.cpp
@@ -4,32 +4,29 @@
 #define N 7
 int main(){
 
- /* 
+ /*
     for (int f=0; f<7; f++){ fila
             printf("***");
         printf("\n");
-    
+
     for (int c=0; c<7; c++) columna
             printf("*");
         printf("\n");
-    } 
+    }
   */
 
 printf(" Ajedrez\n");
 
 for(int f=0; f<N; f++){
    for (int c=0; c<N; c++)
-        if (f==0, f==N-1, c==0,c==N-1) 
-          printf(" ");
-        else 
           printf("*");
-        printf(" *");
+      /*  printf(" "); */
 	printf("\n");
 
-/*for (int f=0; f>3; f++)
+for (int f=0; f>3; f++)
    for (int c=0; c=N; c++)
         printf(" ");
-        printf("\n");*/
+        printf("\n");
 }
 
 
---
+++ clase/sumacelda.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 20
+
+int main(){
+    int [N] = 1
+
+
+        return EXIT_SUCCESS;
+
+}
