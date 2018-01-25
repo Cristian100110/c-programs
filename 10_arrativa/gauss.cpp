47b5cbfaaaa7c83ac377349ff16737375a50278e
cristian100110 <cristiandavid100110@gmail.com>
2018-01-25 11:29:54 +0100

    determinante

---
+++ 10_arrativa/gauss.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 0
+
+
+int main(){
+
+    /*
+     Normalizamos la fila 0
+        div f:0 / c:0
+     Apunto coef
+     Busco 0 en las col 0 de todas las filas
+
+      */
+
+    double norm_f [N][N] = {
+
+        {2, 3, 4},
+        {4, 7, 1},
+        {6, 2, 3}
+
+    };
+
+    for (double f=0; f)
+
+    return EXIT_SUCCESS;
+
+}
